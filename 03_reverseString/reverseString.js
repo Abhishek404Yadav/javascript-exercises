45563d2cb0eceb38bde885170990a0df044d65f2
Abhishek Yadav <abhiyadav.ce@gmail.com>
2022-10-27 04:34:49 +0530

    Completed the 3rd Excercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(name) {
+    let str=name;
+    let str1="";
+    for(let i=str.length-1;i>=0;i--){
+        str1=str1+str[i];
+    }
+    return str1;
 };
 
 // Do not edit below this line
