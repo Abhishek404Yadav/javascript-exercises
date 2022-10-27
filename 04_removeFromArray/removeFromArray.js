41bffa74be4e92dd2c75033633d9e8df2340039c
Abhishek Yadav <abhiyadav.ce@gmail.com>
2022-10-28 03:31:36 +0530

    This method is partially completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...args) {
+    let arr=args[0];
+    for(let i=0; i<=arr.length;i++){
+        for(let j=1;j<=args.length;j++){
+             if(arr[i]===args[j]){
+                arr.splice(i,1);
+             }
+        }
+    }
+        return arr;
 };
 
 // Do not edit below this line
