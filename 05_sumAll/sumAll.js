d36d8aa7f5b8c6d28f037ea044fe7870fe820ad8
Abhishek Yadav <abhiyadav.ce@gmail.com>
2022-10-28 05:21:15 +0530

    Completed Excercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,15 @@
-const sumAll = function() {
+const sumAll = function(n ,m) {
+    let sum=0;
+    if( !('number'===typeof n )||!("number"===typeof m)) return "ERROR";
+    if(n<0||m<0) return "ERROR";
+        if(n>m){
+            sum = (n*(n+1)/2)-(m*(m-1)/2);
+        }
+        else if(m>n){
+            sum = (m*(m+1)/2)-(n*(n-1)/2);
+        }
+        return sum;
+
 
 };
 
