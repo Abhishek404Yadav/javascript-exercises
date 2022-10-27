7926438be7b9a60ff29c1e26626cde4beb01c916
Abhishek Yadav <abhiyadav.ce@gmail.com>
2022-10-28 05:29:10 +0530

    Made the code more readable

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,17 +1,17 @@
-const sumAll = function(n ,m) {
+const sumAll = function(min ,max) {
     let sum=0;
-    if( !('number'===typeof n )||!("number"===typeof m)) return "ERROR";
-    if(n<0||m<0) return "ERROR";
-        if(n>m){
-            sum = (n*(n+1)/2)-(m*(m-1)/2);
+    if( !('number'===typeof min) ||!("number"===typeof max)) return "ERROR";
+    if(min<0||max<0) return "ERROR";
+        //Swapaing
+        if(min>max) { // min=5,max=2
+           max=min+max; // max=5+2(max=7)
+           min=max-min;// min=7-5(min=2)
+           max=max-min// max=7-2(max=5)
         }
-        else if(m>n){
-            sum = (m*(m+1)/2)-(n*(n-1)/2);
-        }
+        sum = (max*(max+1)/2)-(min*(min-1)/2);
         return sum;
 
 
 };
-
 // Do not edit below this line
 module.exports = sumAll;
