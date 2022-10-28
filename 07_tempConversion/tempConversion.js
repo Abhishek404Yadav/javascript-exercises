416fd5f85172d5822f87a5666325053b1ebe2a3d
Abhishek Yadav <abhiyadav.ce@gmail.com>
2022-10-28 06:45:15 +0530

    completed excercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,12 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+return Math.round((f-32)*(5/9)*10)/10;
+// 1 deciaml tak round off karne ke liye 10 se multiply kiya ans ko to uska roud off 10 x ho gya 
+//fir uske result ko 10 se divide kar diya to 1 decimal aa gya 2 deciaml ke liye 100 use kro and so on
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  return Math.round(((c*9/5)+32)*10)/10;
+  
 };
 
 // Do not edit below this line
