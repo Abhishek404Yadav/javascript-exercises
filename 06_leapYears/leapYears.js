45ea45c7d4ba844337cf39c51754d92e6a374b3a
Abhishek Yadav <abhiyadav.ce@gmail.com>
2022-10-28 06:14:16 +0530

    added coments for understanding

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,7 @@
 const leapYears = function(year) {
   return  (year%4===0)&&(!(year % 100==0)||year%400===0);
+  //       T and (T||T),(T||F),(F||T)
+  //  (F||F)=> isme wo 100 se divide ho ja ra h to T negate F then 400 se divide nhi ho raha then F
 };
 
 // Do not edit below this line
